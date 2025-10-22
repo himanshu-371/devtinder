613d6a6a00d13e25b1b5fcc7e31dba58ff8f70e1
himanshu-371 <himanshuchauhan371@gmail.com>
2025-10-22 12:18:01 +0530

    initialized the devtinder backend

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/t", (req, res)=>{
+    res.send("Namaste from the server!")
+})
+
+app.use("/", (req, res)=>{
+    res.send("Hello from the server!")
+})
+
+app.listen(3000, ()=>{
+    console.log("Server is successfully listening on port 3000.")
+});
+
